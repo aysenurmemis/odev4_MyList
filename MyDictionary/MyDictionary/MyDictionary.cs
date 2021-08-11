1e96151f1c97c5f92c10dab327bbe6deb225b6a9
Ayşenur MEMİŞ <83470364+aysenurmemis@users.noreply.github.com>
2021-08-11 11:52:30 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] key;
+        TValue[] value;
+
+        public MyDictionary()
+        {
+            key = new TKey[0];
+            value = new TValue[0];
+        }
+
+        public void Add(TKey item1,TValue item2)
+        {
+            TKey[] temparrakey = key;
+            TValue[] temparrayvalue = value;
+
+            key = new TKey[key.Length + 1];
+            value = new TValue[value.Length + 1];
+
+            for (int i = 0; i < temparrakey.Length; i++)
+            {
+                key[i] = temparrakey[i];
+                value[i] = temparrayvalue[i];
+            }
+
+            key[key.Length - 1] = item1;
+            value[value.Length - 1] = item2;
+
+            Console.WriteLine(item1 + " numaralı " + item2 + " adlı kullanıcı sisteme eklendi.");
+
+        }
+
+
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary1 = new MyDictionary<int, string>();
+            myDictionary1.Add(1, "Aysenur");
+
+            MyDictionary<int, string> myDictionary2 = new MyDictionary<int, string>();
+            myDictionary2.Add(2, "Mervenur");
+        }
+    }
+}
